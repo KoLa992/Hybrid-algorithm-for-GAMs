6ce1b507766d472414419ed2feed183e057af423
László Kovács <68534070+KoLa992@users.noreply.github.com>
2022-05-29 15:02:00 +0200

    Add files via upload

--- Concrete-Data/HybridApplication_ConcreteData.R
+++ Concrete-Data/HybridApplication_ConcreteData.R
@@ -30,7 +30,7 @@ test_index <- 38
 library(parallel)
 magok<-detectCores()
 
-runnumber=30
+runnumber=10
 eredmenyek<-matrix(nrow = runnumber,ncol = 6)
 
 for (i in 1:runnumber) {
@@ -50,15 +50,45 @@ write.table(eredmenyek, file = paste("resHibrid_",test_index,".csv", sep = ""), 
           quote = FALSE, row.names = FALSE)
 
 #get the best modell --> row index of "eredmenyek" needs to be updated!!!
-best.mod<-ModellEpit_B(as.numeric(strsplit(eredmenyek[1,1],",")[[1]]),ConcreteTrain_X,ConcreteTrain_Y,
+best.mod<-ModellEpit_B(as.numeric(strsplit(eredmenyek[3,1],",")[[1]]),ConcreteTrain_X,ConcreteTrain_Y,
                        "gaussian",c(""),magok-1)
 
 # Diagnostics and performance of the GAM proposed by the Hybrid algorithm
 summary(best.mod)
-plot(best.mod,page=1)
+plot(best.mod,page=1,scheme=1,unconditional=TRUE)
 
 concurvity(best.mod)
 gam.check(best.mod)
 
+# Outlier effect - Cook distance
+
+cook <- cooks.distance(best.mod)
+length(cook[cook > 1]) # No influential observations
+
+# Performance - R^2
+
 ConcreteTest_HybridPredY<-predict(best.mod,newdata=test)
 Rsquared_Hybrid<-cor(ConcreteTest_Y,ConcreteTest_HybridPredY)^2 #84.363%
+
+# Other performance metrics
+
+lsa::cosine(as.vector(ConcreteTest_HybridPredY), ConcreteTest_Y) # 0.9841959
+
+MLmetrics::RMSE(ConcreteTest_HybridPredY, ConcreteTest_Y) # 6.799805
+
+MLmetrics::RMSPE(ConcreteTest_HybridPredY, ConcreteTest_Y) # 0.2672085
+
+# Handling the possible negative values for logarithm
+AbsoluteMin <- min(min(ConcreteTest_HybridPredY), min(ConcreteTest_Y))
+
+if (AbsoluteMin < 0) {
+  MLmetrics::RMSLE((ConcreteTest_HybridPredY - AbsoluteMin + 1), (ConcreteTest_Y - AbsoluteMin + 1))
+} else {
+  MLmetrics::RMSLE(ConcreteTest_HybridPredY, ConcreteTest_Y)
+} # 0.2149622
+
+MLmetrics::MAE(ConcreteTest_HybridPredY, ConcreteTest_Y) # 5.412294
+
+MLmetrics::MAPE(ConcreteTest_HybridPredY, ConcreteTest_Y) # 0.1904206
+
+sum(log(cosh(ConcreteTest_HybridPredY - ConcreteTest_Y)))/length(ConcreteTest_Y) # 4.770746
